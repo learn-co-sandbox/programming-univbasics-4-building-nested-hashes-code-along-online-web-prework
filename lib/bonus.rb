f362bee1deda7f0517c3f43bdba35bb26ab4ffcc
Nikelson Exantus <nikelsone1981@hotmail.com>
2019-10-29 20:42:35 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -24,8 +24,8 @@ def bonus
 epic_tragedy[:montague][:patriarch][:name] = "Michael Jordan"
 puts  epic_tragedy[:montague][:hero][:status] = "dead"
 puts epic_tragedy[:capulet][:heroine][:status] = "dead"
-[:montague][:hero][:status] = epic_tragedy
-[:capulet][:heroine][:status] = epic_tragedy
+#[:montague][:hero][:status] = epic_tragedy
+#[:capulet][:heroine][:status] = epic_tragedy
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy[status]
 end
